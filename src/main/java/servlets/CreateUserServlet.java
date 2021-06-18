4296421937f481f6f7088b4785bed29c04163d7d
VLADYSLAVA UVAROVA <vuvarova@outlook.com>
2021-06-18 12:05:30 +0300

    first try

---
+++ src/main/java/servlets/CreateUserServlet.java
@@ -0,0 +1,71 @@
+package servlets;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@WebServlet(urlPatterns = "/addServlet")
+
+public class CreateUserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private Connection connection;
+    PreparedStatement statement;
+
+    public void init(ServletConfig config){
+        try{
+            ServletContext context = config.getServletContext();
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(
+                    context.getInitParameter("dbUrl"),
+                    context.getInitParameter("dbUser"),
+                    context.getInitParameter("dbPassword") );
+            statement = connection.prepareStatement("insert into person(first_name, last_name, age, email) values(?,?,?,?)");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        int age = Integer.parseInt(request.getParameter("age"));
+        String email = request.getParameter("email");
+
+        try {
+            statement.setString(1, firstName);
+            statement.setString(2, lastName);
+            statement.setInt(3, age);
+            statement.setString(4, email);
+            statement.executeUpdate();
+
+            out.print("User created");
+
+        } catch (SQLException e) {
+            out.println("Error Creating the User");
+            e.printStackTrace();
+        }
+    }
+
+
+    public void destroy() {
+        try {
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
